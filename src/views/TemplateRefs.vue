f568755cb3e727e5b3b05b6691c8d7a11cc9e805
Ibimina <ibimina.c.hart@gmail.com>
2022-12-14 20:01:45 +0100

    docs: change data state using refs and methods

--- src/views/TemplateRefs.vue
+++ src/views/TemplateRefs.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="about">
-    <h1>Event listeners using template refs</h1>
-    <p>{{ text }}</p>
-    <p ref="events">hi</p>
-    <button @mousedown="handlemouse('mouse down')">mouse down</button>
-    <button @mouseup="handlemouse('mouse up')">mouse up</button>
-    <button @mouseenter="handlemouse('mouse enter')">mouse enter</button>
-    <button @mouseleave="handlemouse('mouse leave')">mouse leave</button>
-    <button @mousemove="handlemouse('mouse move')">mouse move</button>
+  <div class="template-refs">
+    <div>
+      <h1>Change text using template refs with Event listeners</h1>
+      <p>{{ text }}</p>
+      <p ref="events" class="mouse-event">hi</p>
+      <div class="mouse-wrapper">
+        <button @mousedown="handlemouse('mouse down')">mouse down</button>
+        <button @mouseup="handlemouse('mouse up')">mouse up</button>
+        <button @mouseenter="handlemouse('mouse enter')">mouse enter</button>
+        <button @mouseleave="handlemouse('mouse leave')">mouse leave</button>
+        <button @mousemove="handlemouse('mouse move')">mouse move</button>
+      </div>
+    </div>
+
+    <div>
+      <h1>Change text using Event listeners</h1>
+      <p>State mangement in vue</p>
+      <p class="mouse-event">{{ data }}</p>
+      <div class="mouse-wrapper">
+        <button @mousedown="handleevent('mouse down')">mouse down</button>
+        <button @mouseup="handleevent('mouse up')">mouse up</button>
+        <button @mouseenter="handleevent('mouse enter')">mouse enter</button>
+        <button @mouseleave="handleevent('mouse leave')">mouse leave</button>
+        <button @mousemove="handleevent('mouse move')">mouse move</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return { text: "mouse events" };
+    return { text: "mouse events", data: "data state " };
   },
   methods: {
     handlemouse(word) {
       this.$refs.events.textContent = word;
+    },
+    handleevent(word) {
+      this.data = word;
     },
   },
 };
